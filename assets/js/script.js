42ac0726627bb015e219a06ff49999b4b2fb5d15
unknown <c00290928@itcarlow.ie>
2024-05-02 16:33:57 +0100

    added ineractivity and some text in accesibility

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 function scrollDown(){
     window.scrollTo(0, document.body.scrollHeight + 2000);
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    $(document).ready(function(){
+    $("#good-toggle").click(function(){
+      $("#good1").toggle();
+      $("#good2").toggle();
+    });});
+
+    $(document).ready(function(){
+    $("#bad-toggle").click(function(){
+      $("#bad1").toggle();
+      $("#bad2").toggle();
+    });});
+});
