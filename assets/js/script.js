43669df8b075e4a596fe651645d9477ec1ab1250
Caitlin-Brennan <116352640+Caitlin-Brennan@users.noreply.github.com>
2024-05-02 16:32:04 +0100

    responsive for mobile edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 function scrollDown(){
     window.scrollTo(0, document.body.scrollHeight + 2000);
 }
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") 
+    {
+        x.style.display = "none";
+    } else 
+    {
+        x.style.display = "block";
+    }
+}
